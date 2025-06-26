957c4996c0a9cf747d354bde77145e727c34aa91
Richard Wilkes <wilkes@me.com>
2025-06-26 15:51:13 -0700

    Update copyright and bump version

--- copyright.go
+++ copyright.go
@@ -1,4 +1,4 @@
-// Copyright (c) 1996-2024 by Richard A. Wilkes. All rights reserved.
+// Copyright (c) 2016-2025 by Richard A. Wilkes. All rights reserved.
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, version 2.0. If a copy of the MPL was not distributed with
@@ -41,7 +41,7 @@ var (
 
 func main() {
 	cmdline.AppName = "Copyright"
-	cmdline.AppVersion = "1.0.2"
+	cmdline.AppVersion = "1.1.0"
 	cmdline.CopyrightStartYear = "2016"
 	cmdline.CopyrightHolder = "Richard A. Wilkes"
 	cmdline.License = "Mozilla Public License 2.0"
